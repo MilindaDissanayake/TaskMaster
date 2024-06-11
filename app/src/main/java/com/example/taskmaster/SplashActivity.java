cc6a19c31631af0c9523ec2bf3b2004b135870a3
MilindaDissanayake <150835052+MilindaDissanayake@users.noreply.github.com>
2024-06-12 01:38:46 +0530

    splash screen complete
    backend and front end complete

--- app/src/main/java/com/example/taskmaster/SplashActivity.java
+++ app/src/main/java/com/example/taskmaster/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.taskmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Button getStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        getStart = findViewById(R.id.getStartbtn);
+        getStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivities(new Intent[]{intent});
+            }
         });
     }
 }
