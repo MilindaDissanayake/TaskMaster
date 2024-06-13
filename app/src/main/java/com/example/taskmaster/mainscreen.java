abf851aee9869fc371235f64b18696c09e76417a
MilindaDissanayake <150835052+MilindaDissanayake@users.noreply.github.com>
2024-06-14 04:57:58 +0530

    user info screen connect

--- app/src/main/java/com/example/taskmaster/mainscreen.java
+++ app/src/main/java/com/example/taskmaster/mainscreen.java
@@ -1,7 +1,9 @@
 package com.example.taskmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -16,5 +18,15 @@ public class mainscreen extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mainscreen);
 
+        TextView btnUserinfo = findViewById(R.id.btnUserinfo);
+        btnUserinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainscreen.this, Userinfo.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
