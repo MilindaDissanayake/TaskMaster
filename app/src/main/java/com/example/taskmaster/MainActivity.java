76521f65d5a6b22a6dcf89903bcd72d57d140733
MilindaDissanayake <150835052+MilindaDissanayake@users.noreply.github.com>
2024-06-13 12:42:12 +0530

    login + sign up
    login and sign up screen ui done

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.taskmaster;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +18,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView Signup;
+    EditText Email;
+    EditText Password;
+    Button Login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Signup=findViewById(R.id.textSignup);
+        Email =findViewById(R.id.editTextEmail);
+        Password =findViewById(R.id.editTextPw);
+        Login = findViewById(R.id.Login);
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SignUpscreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
 
 
     }
